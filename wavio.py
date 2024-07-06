d38fc53b29a87d7955c9793d23dfb97a903dcfc7
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-06 16:54:58 +0900

    Update: version

--- wavio.py
+++ wavio.py
@@ -1,11 +1,10 @@
 import numpy as np
 from scipy.io import wavfile
-from typing import Tuple  # TODO upgrade python (>= 3.9)
 
 
-def read_wave_16bit(filename: str, is_mono: bool =False, seed: int =0) -> Tuple[np.ndarray, int]:
+def read_wave_16bit(filename: str, is_mono: bool =False, seed: int =0) -> tuple[np.ndarray, int]:
     sr, data = wavfile.read(filename)
-    data = data.astype(np.float)  # (sample, channel)
+    data = data.astype(np.float64)  # (sample, channel)
     assert (len(data.shape) == 1 and is_mono) or (len(data.shape) == 2 and not is_mono)
     assert np.all(-32768 <= data) and np.all(data <= 32767)
 
