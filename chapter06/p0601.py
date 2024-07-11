3ddffd7eee44d0fe457718065ee87d379d8d6718
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-11 23:52:31 +0900

    Update: list06.01

--- chapter06/p0601.py
+++ chapter06/p0601.py
@@ -1,22 +1,23 @@
 import sys
 sys.path.append("..")
 import numpy as np
-from osc import sine
+from osc import *
 from wavio import write_wave_16bit
 
 
 if __name__ == "__main__":
-    sr: int = 44100
-    sec: float = 1.0
+    for name, func in {'sin': sine}.items():
+        sr: int = 44100
+        sec: float = 1.0
 
-    f: float = 440.0
-    vco: np.ndarray = sine(fs=f, sr=sr, sec=sec)
-    vca: np.ndarray = np.ones(int(sec*sr))
-    ys: np.ndarray = vco * vca
+        f: float = 440.0
+        vco: np.ndarray = func(fs=f, sr=sr, sec=sec)
+        vca: np.ndarray = np.ones(int(sec*sr))
+        ys: np.ndarray = vco * vca
 
-    blank: float = 1.0
-    vol: float = 0.5
-    zs: np.ndarray = np.zeros(int(blank*sr))
-    out: np.ndarray = vol * np.concatenate([zs, ys, zs])
+        blank: float = 1.0
+        vol: float = 0.5
+        zs: np.ndarray = np.zeros(int(blank*sr))
+        out: np.ndarray = vol * np.concatenate([zs, ys, zs])
 
-    write_wave_16bit(out, sr, "p0601_output.wav", is_mono=True)
+        write_wave_16bit(out, sr, f"p0601_{name}.wav", is_mono=True)
