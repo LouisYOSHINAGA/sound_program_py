1cb5ec2f0b82a4ab16e7c40a129b45df3c317c87
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-17 22:24:39 +0900

    Update: oscillator

--- osc.py
+++ osc.py
@@ -14,22 +14,17 @@ def sawtooth(fs: float|np.ndarray, sr: int, sec: float|None =None) -> np.ndarray
     if isinstance(fs, (int, float)):
         assert sec is not None
         fs = np.full(int(sec*sr), fs)
-    assert isinstance(fs, np.ndarray)
-    assert np.all(fs != 0)
-    lastf: np.ndarray = np.array([fs[-1]])
-    assert isinstance(lastf, np.ndarray)
-    fs = np.concatenate([np.zeros(1), fs[:-1]])
-    fsts: np.ndarray = np.cumsum(fs / sr) % 1
-    ys: np.ndarray = -2 * fsts + 1
-    fs = np.concatenate([fs[1:], lastf])
+    assert isinstance(fs, np.ndarray) and np.all(fs != 0)
+    phases: np.ndarray = np.cumsum(np.concatenate([[0], fs[:-1]/sr])) % 1
+    ys: np.ndarray = -2 * phases + 1
 
     ds: np.ndarray = np.zeros(len(ys))
-    is_period_end: np.ndarray = fsts[1:] - fsts[:-1] < 0
+    is_period_end: np.ndarray = phases[1:] - phases[:-1] < 0
     is_period_end = np.append(is_period_end, False)
-    ds[is_period_end] = ((fsts[is_period_end] - 1)*sr/fs[is_period_end] + 1)**2
+    ds[is_period_end] = ((phases[is_period_end] - 1)*sr/fs[is_period_end] + 1)**2
     is_period_start: np.ndarray = np.roll(is_period_end, 1)
     is_period_start[0] = True
-    ds[is_period_start] = - (fsts[is_period_start]*sr/fs[is_period_start] - 1)**2
+    ds[is_period_start] = - (phases[is_period_start]*sr/fs[is_period_start] - 1)**2
     return ys + ds
 
 
