a72a5a3cb83f7e580a6d322cfb8ec55c47bcfa30
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-20 16:56:41 +0900

    Add: list06.05

---
+++ chapter06/p0606.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append("..")
+import numpy as np
+from osc import sine
+from env import adsr
+from wavio import write_wave_16bit
+
+
+if __name__ == "__main__":
+    sr: int = 44100
+    f0: float = 440
+    gate: float = 3.0
+    sec: float = 4.0
+
+    partial_params: list[dict[str, dict[str, float]]] = [
+        {'vco': {'A': 0.0, 'D': 0.0, 'S': 1.0, 'R': 0.0, 'gate': sec, 'dur': sec, 
+                 'offset': f0, 'depth': 0.0},
+         'vca': {'A': 0.01, 'D': 0.0, 'S': 1.0, 'R': 0.01, 'gate': gate, 'dur': sec, 
+                 'offset': 0.0, 'depth': 1.0}},
+        {'vco': {'A': 0.0, 'D': 0.0, 'S': 1.0, 'R': 0.0, 'gate': sec, 'dur': sec, 
+                 'offset': 2*f0, 'depth': 0.0},
+         'vca': {'A': 0.01, 'D': 0.0, 'S': 1.0, 'R': 0.01, 'gate': gate, 'dur': sec, 
+                 'offset': 0.0, 'depth': 1.0}},
+    ]
+
+    ys: np.ndarray = np.empty((len(partial_params), int(sec*sr)))
+
+    vcos: np.ndarray = np.empty((len(partial_params), int(sec*sr)))
+    for i, partial_param in enumerate(partial_params):
+        p: dict[str, float] = partial_param['vco']
+        vcos[i] = p['offset'] \
+                + p['depth'] * adsr(sec_A=p['A'], sec_D=p['D'], amp_S=p['S'], sec_R=p['R'], 
+                                    sec_gate=p['gate'], dur=p['dur'], sr=sr)
+        ys[i] = sine(fs=vcos[i], sr=sr)
+
+    vcas: np.ndarray = np.empty((len(partial_params), int(sec*sr)))
+    for i, partial_param in enumerate(partial_params):
+        p: dict[str, float] = partial_param['vca']
+        vcas[i] = p['offset'] \
+                + p['depth'] * adsr(sec_A=p['A'], sec_D=p['D'], amp_S=p['S'], sec_R=p['R'], 
+                                    sec_gate=p['gate'], dur=p['dur'], sr=sr)
+        ys[i] *= vcas[i]
+
+    y: np.ndarray = np.mean(ys, axis=0)
+
+    blank: float = 1.0
+    vol: float = 0.5
+    z: np.ndarray = np.zeros(int(blank*sr))
+    out: np.ndarray = vol * np.concatenate([z, y, z])
+
+    write_wave_16bit(out, sr, f"p0606_output.wav", is_mono=True)
