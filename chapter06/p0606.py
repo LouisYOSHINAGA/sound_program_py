4e64ead7967219d4666d54a15093b237ea3ad209
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-07-22 15:15:12 +0900

    Refactor: list06.05

--- chapter06/p0606.py
+++ chapter06/p0606.py
@@ -19,16 +19,15 @@ if __name__ == "__main__":
         {'vco': {'A': 0.0, 'D': 0.0, 'S': 1.0, 'R': 0.0, 'gate': sec, 'dur': sec, 'offset': 2*f0, 'depth': 0.0},
          'vca': {'A': 0.01, 'D': 0.0, 'S': 1.0, 'R': 0.01, 'gate': gate, 'dur': sec, 'offset': 0.0, 'depth': 1.0}},
     ]
+    adsr_args: list[str] = inspect.signature(adsr).parameters.keys()
 
     vcos: np.ndarray = np.empty((len(partial_params), int(sec*sr)))
     vcas: np.ndarray = np.empty((len(partial_params), int(sec*sr)))
     for i, partial_param in enumerate(partial_params):
         po: dict[str, float] = partial_param['vco']
-        vcos[i] = po['offset'] + po['depth'] * adsr(**{k: v for k, v in po.items()
-                                                    if k in inspect.signature(adsr).parameters.keys()})
+        vcos[i] = po['offset'] + po['depth'] * adsr(**{k: v for k, v in po.items() if k in adsr_args})
         pa: dict[str, float] = partial_param['vca']
-        vcas[i] = pa['offset'] + pa['depth'] * adsr(**{k: v for k, v in pa.items()
-                                                    if k in inspect.signature(adsr).parameters.keys()})
+        vcas[i] = pa['offset'] + pa['depth'] * adsr(**{k: v for k, v in pa.items() if k in adsr_args})
     y: np.ndarray = np.mean(vcas * sine(fs=vcos, sr=sr), axis=0)
 
     blank: float = 1.0
