89b66c348cc366b8e3b1031b6b9416060a8da755
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-18 23:28:57 +0900

    Add: marimba

--- chapter09/instruments/common.py
+++ chapter09/instruments/common.py
@@ -1,5 +1,11 @@
+import numpy as np
+
 A4_FREQ: float = 440.0
 A4_NOTE: int = 69
 
 def calc_freq(note_no: int) -> float:
     return A4_FREQ * 2 ** ((note_no - A4_NOTE) / 12)
+
+def gen_noise(n_sample: int, seed: int =0) -> np.ndarray:
+    rng: np.random.Generator = np.random.default_rng(seed)
+    return 2 * (rng.random(size=n_sample) - 0.5)
