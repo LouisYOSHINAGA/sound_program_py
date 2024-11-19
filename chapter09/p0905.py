91c5d778d78b73cf6d7ddd27d0c7d9767d551545
Louis YOSHINAGA <9dutat.e@gmail.com>
2024-11-19 23:42:23 +0900

    Add: xylophone

---
+++ chapter09/p0905.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("..")
+import numpy as np
+from instruments.percussions import xylophone
+from wavio import write_wave_16bit
+
+
+def main(note_no: int, velocity: int, gate: float =0.1, duration: float =1.8,
+         blanks: list[float] =[1.0, 2.0], sr: int =44100) -> None:
+    x: np.ndarray = xylophone(note_no, velocity, gate, duration, sr)
+    w: np.ndarray = np.zeros(int(blanks[0] * sr))
+    z: np.ndarray = np.zeros(int(blanks[1] * sr))
+    y: np.ndarray = np.concatenate([w, x, z])
+    write_wave_16bit(y, sr=sr, filename="p0905_xylophone.wav", is_mono=True)
+
+
+if __name__ == "__main__":
+    main(note_no=65, velocity=100)
---
+++ instruments/percussions.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("..")
+import numpy as np
+from env import adsr
+from biquad import biquad_filter
+import instruments.utils as utils
+
+
+def xylophone(note_no: int, velocity: int, gate: float, duration: float =1.8, sr: int =44100) -> np.ndarray:
+    freq: float = utils.calc_freq(note_no)
+    params: dict[str, dict[str, float]] = {
+        'vcf': {'A': 0.0, 'D': 0.2, 'S': 0.0, 'R': 0.2, 'gate': gate, 'dur': duration, 'offset': 500, 'depth': 2000},
+        'vca': {'A': 0.0, 'D': 0.8, 'S': 0.0, 'R': 0.8, 'gate': gate, 'dur': duration, 'offset':   0, 'depth':    1},
+    }
+
+    adsr_args: list[str] = utils.get_func_kwargs(adsr)
+    vco: np.ndarray = utils.noise(int(duration * sr))
+    vcf: np.ndarray = params['vcf']['offset'] \
+                    + params['vcf']['depth'] * adsr(**{k: v for k, v in params['vcf'].items() if k in adsr_args})
+    vca: np.ndarray = params['vca']['offset'] \
+                    + params['vca']['depth'] * adsr(**{k: v for k, v in params['vca'].items() if k in adsr_args})
+
+    z: np.ndarray = biquad_filter(data=vco, filter_type="lowpass", fc=vcf, Q=1/np.sqrt(2), sr=sr)
+    z0: np.ndarray = biquad_filter(data=z, filter_type="bandpass", fc=freq, Q=200, sr=sr)
+    z1: np.ndarray = biquad_filter(data=z, filter_type="bandpass", fc=3*freq, Q=200, sr=sr)
+    z2: np.ndarray = biquad_filter(data=z, filter_type="bandpass", fc=6.5*freq, Q=200, sr=sr)
+    z3: np.ndarray = biquad_filter(data=z, filter_type="bandpass", fc=10*freq, Q=200, sr=sr)
+    y: np.ndarray = vca * (z0 + z1 + z2 + z3)
+    return (velocity / 127) / np.max(np.abs(y)) * y
